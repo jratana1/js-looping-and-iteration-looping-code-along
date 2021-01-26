758ceb61aa358657d41e77bf98f9c321db30938f
John Ratana <jratana1@gmail.com>
2021-01-25 23:13:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let answer = []
+    for (let i = 0; i < names.length; i++) {
+        answer.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      }
+    return answer
+}
+
+function countDown(integer){
+    let count = integer;
+    while (count >= 0) {
+        console.log(count--);
+    } 
+}
